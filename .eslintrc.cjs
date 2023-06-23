9dd5d89e9ac141c9f8178689bfb263a691e817a7
ruslan.viaznykov <ruslan.viaznykov@innovecs.com>
2023-06-23 12:32:48 +0300

    migrate from create-react-app to vite

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,27 +1,15 @@
 module.exports = {
-  parser: '@babel/eslint-parser',
-  parserOptions: {
-    requireConfigFile: false,
-    babelOptions: {
-      presets: ['@babel/preset-react'],
-    },
-  },
-  plugins: [
-    'no-inline-styles',
-  ],
+  env: { browser: true, es2020: true },
   extends: [
     'airbnb',
     'airbnb/hooks',
   ],
-  env: {
-    browser: true,
-    node: true,
-    jest: true,
-    mocha: true,
-  },
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
   rules: {
+    'react-refresh/only-export-components': 'warn',
     'react-hooks/exhaustive-deps': 0,
-    'no-inline-styles/no-inline-styles': 2,
     'no-param-reassign': [2, { props: false }],
     'max-len': [2, { code: 120 }],
     'react/destructuring-assignment': [2, 'never', { ignoreClassFields: true }],
@@ -29,4 +17,4 @@ module.exports = {
     'react/jsx-key': [2, { checkFragmentShorthand: true }],
     'react/jsx-no-useless-fragment': [2, { allowExpressions: true }],
   },
-};
+}
---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    target: 'modules', // es2015
+    assetsDir: 'static',
+    outDir: './build',
+    manifest: true,
+    sourcemap: true,
+  },
+  plugins: [react()],
+  server: {
+    port: 8080,
+    cors: false,
+  },
+});
