e362c1709bb85276c148d8974ad05627e996699d
IvettaDashkova <ivettafsd@gmail.com>
2024-05-07 11:29:26 +0200

    test

--- src/modules/@common/components/Header/Header.jsx
+++ src/modules/@common/components/Header/Header.jsx
@@ -8,6 +8,7 @@ function Header() {
     <header>
       <img src="src/img/menu.svg" alt="" />
       <img src="src/img/logo.svg" alt="" />
+          <img src="src/img/logo.svg" alt="" />
       <a href="#contacts">{t('common').header.contactUs }</a>
       <LngSwitcher/>
     </header>
