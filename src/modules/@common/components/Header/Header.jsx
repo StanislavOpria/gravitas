48c1196a796dcf96373906289a923b91f0f69f04
IvettaDashkova <ivettafsd@gmail.com>
2024-05-07 11:29:00 +0200

    test

--- src/modules/@common/components/Header/Header.jsx
+++ src/modules/@common/components/Header/Header.jsx
@@ -6,8 +6,8 @@ function Header() {
     const { t } = useTranslation();
   return (
     <header>
-      {/* <img src="src/img/menu.svg" alt="" />
-      <img src="src/img/logo.svg" alt="" /> */}
+      <img src="src/img/menu.svg" alt="" />
+      <img src="src/img/logo.svg" alt="" />
       <a href="#contacts">{t('common').header.contactUs }</a>
       <LngSwitcher/>
     </header>
